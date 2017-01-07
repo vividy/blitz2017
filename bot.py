38b0219f6fd715b6a25373c35489d5921081e566
vividy <vivien.pradelles@epitech.eu>
2017-01-07 08:18:34 -0800

    mindDist

--- bot.py
+++ bot.py
@@ -6,8 +6,28 @@ class Bot:
     pass
 
 class RandomBot(Bot):
-    def move(self, state):
-        game = Game(state)
-        dirs = ['Stay', 'North', 'South', 'East', 'West']
-        pathfinding(game.board, (0,0), (1,0))
-        return choice(dirs)
+	def minDist(self, pos, game):
+		self.burger_loc = (-1, -1)
+		self.fries_locs = (-1, -1)
+		self.heroes_locs = (-1, -1)
+		for k, v in game.burger_locs:
+			burger_loc = (k, v)
+		for k, v in game.fries_locs:
+			fries_locs = (k, v)
+		for k, v in game.heroes_locs:
+			if (k != pos[0] & v != pos[1]):
+				heroes_locs = (k, v)
+		return([burger_loc, fries_locs, heroes_locs])
+
+	def move(self, state):
+		game = Game(state)
+		dirs = ['Stay', 'North', 'South', 'East', 'West']
+		pathfinding(game.board, (0,0), (1,0))
+		print(self.minDist([5,5], game))
+		print(game.board.size)
+		print(game.heroes[0].name)
+		print(game.board.tiles[game.heroes[0].pos['x']][game.heroes[0].pos['y']])
+		# for x in xrange(0,game.board.size):
+		# 	for y in game.board.tiles[x]
+		# 		print("x: " + x + "\ty: " + y + "\tcase: "+ game.board.tiles[x][])
+		return choice(dirs)
