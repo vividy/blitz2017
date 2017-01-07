2f32f467a58391933b06c816ae2712c3b080ee16
leroux_p <leroux_p@epitech.eu>
2017-01-07 15:34:20 +0100

    pathfinding

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 from random import choice
 from game import Game
+import pathfinding
 
 
 class Bot:
     pass
 
-
 class RandomBot(Bot):
     def move(self, state):
         game = Game(state)
         dirs = ['Stay', 'North', 'South', 'East', 'West']
+        pathfinding(game.board, 0, 1)
         return choice(dirs)
---
+++ pathfinding.py
@@ -0,0 +1,15 @@
+import re
+import requests
+
+BASE_URL = "http://game.blitz.codes:8081/pathfinding/direction"
+
+
+def pathfinding( map, start, target)
+    params = {'map': map.tiles, 'size' : map.size, 'start' : start, 'target' : target}
+
+    r = requests.post(server_url + api_endpoint, params, timeout=10*60)
+    if r.status_code == 200:
+        print(r.json())
+    else:
+        print("Error path")
+        print(r.text)
