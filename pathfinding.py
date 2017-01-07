da388687cf3ae25b578fca1600a5d39fcd19bc0d
Bastien Penavayre <bastienPenava@gmail.com>
2017-01-07 09:37:33 -0500

    Missing :

--- pathfinding.py
+++ pathfinding.py
@@ -4,7 +4,7 @@ import requests
 BASE_URL = "http://game.blitz.codes:8081/pathfinding/direction"
 
 
-def pathfinding( map, start, target)
+def pathfinding( map, start, target):
     params = {'map': map.tiles, 'size' : map.size, 'start' : start, 'target' : target}
 
     r = requests.post(server_url + api_endpoint, params, timeout=10*60)
