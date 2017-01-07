5a12ae518a45edbb695b772d9291a5b4929039c5
Bastien Penavayre <bastienPenava@gmail.com>
2017-01-07 09:59:25 -0500

    Functional

--- bot.py
+++ bot.py
@@ -8,6 +8,11 @@ class Bot:
 class RandomBot(Bot):
     def move(self, state):
         game = Game(state)
+        us = None
+        for x in game.heroes:
+            if (x.name == "Rimouski City Gang"):
+                us = x
+                break;
         dirs = ['Stay', 'North', 'South', 'East', 'West']
-        pathfinding(game.board, (0,0), (1,0))
+        res = pathfinding(game.board, (us.pos["x"], us.pos["y"]), (1, 0))
         return choice(dirs)
