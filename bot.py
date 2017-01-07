ba0fa0edbff7bfe299772ac35de73cfd271e4ddd
vividy <vivien.pradelles@epitech.eu>
2017-01-07 08:45:45 -0800

    minDistV1Complet

--- bot.py
+++ bot.py
@@ -1,23 +1,12 @@
 from random import choice
 from game import Game
 from pathfinding import pathfinding
+from dist import minDist
 
 class Bot:
     pass
 
 class RandomBot(Bot):
-	def minDist(self, pos, game):
-		self.burger_loc = (-1, -1)
-		self.fries_locs = (-1, -1)
-		self.heroes_locs = (-1, -1)
-		for k, v in game.burger_locs:
-			burger_loc = (k, v)
-		for k, v in game.fries_locs:
-			fries_locs = (k, v)
-		for k, v in game.heroes_locs:
-			if (k != pos[0] & v != pos[1]):
-				heroes_locs = (k, v)
-		return([burger_loc, fries_locs, heroes_locs])
 
 	# def move(self, state):
 	# 	game = Game(state)
@@ -31,13 +20,14 @@ class RandomBot(Bot):
 	# 	# 	for y in game.board.tiles[x]
 	# 	# 		print("x: " + x + "\ty: " + y + "\tcase: "+ game.board.tiles[x][])
 	# 	return choice(dirs)
-    def move(self, state):
-        game = Game(state)
-        us = None
-        for x in game.heroes:
-            if (x.name == "Rimouski City Gang"):
-                us = x
-                break;
-        dirs = ['Stay', 'North', 'South', 'East', 'West']
-        res = pathfinding(game.board, (us.pos["x"], us.pos["y"]), (1, 0))
-        return choice(dirs)
+	def move(self, state):
+		game = Game(state)
+		us = None
+		for x in game.heroes:
+			if (x.name == "Rimouski City Gang"):
+				us = x
+				break;
+		print(minDist([5,5], game))
+		dirs = ['Stay', 'North', 'South', 'East', 'West']
+		res = pathfinding(game.board, (us.pos["x"], us.pos["y"]), (1, 0))
+		return choice(dirs)
--- dist.py
+++ dist.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
 import sys
 import requests
 
 
-def dist(a, b):
-    return (math.sqrt(
-                    (((b[0] - a[0])) * (b[0] - a[0]))
-                    + ((b[1] - a[1]) * (b[1] - a[1]))
-                    ))
+def minDist(pos, game):
+	burger_loc = (-1, -1)
+	burger_dist = -1
+	fries_loc = (-1, -1)
+	fries_dist = -1
+	heroes_loc = (-1, -1)
+	heroes_dist = -1
+	for k, v in game.burger_locs:
+		if (burger_dist == -1):
+			burger_loc = (k, v)
+			burger_dist = dist(pos, (k, v))
+		elif burger_dist > dist(pos, (k, v)):
+			burger_loc = (k, v)
+			burger_dist = dist(pos, (k, v))
+	for k, v in game.fries_locs:
+		if (fries_dist == -1):
+			fries_loc = (k, v)
+			fries_dist = dist(pos, (k, v))
+		elif fries_dist > dist(pos, (k, v)):
+			fries_loc = (k, v)
+			fries_dist = dist(pos, (k, v))
+	for k, v in game.heroes_locs:
+		if (k != pos[0] & v != pos[1]):
+			if (heroes_dist == -1):
+				heroes_loc = (k, v)
+				heroes_dist = dist(pos, (k, v))
+			elif heroes_dist > dist(pos, (k, v)):
+				heroes_loc = (k, v)
+				heroes_dist = dist(pos, (k, v))
+	return([burger_loc, fries_loc, heroes_loc])
 
-print(dist([10, 10], [10, 10]))
+def dist(a, b):
+	return ((((b[0] - a[0])) * (b[0] - a[0])) + ((b[1] - a[1]) * (b[1] - a[1])))
